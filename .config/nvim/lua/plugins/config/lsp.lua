6531040a4fc60570753b94d18ff2af7c18aeed5d
Phelipe Teles <phelipe_teles@hotmail.com>
2025-10-29 22:00:12 -0300

    Handle nil in global LSP servers on_attach handler

--- .config/nvim/lua/plugins/config/lsp.lua
+++ .config/nvim/lua/plugins/config/lsp.lua
@@ -198,6 +198,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client then
+      return
+    end
 
     local opts = { buffer = ev.buf }
 
